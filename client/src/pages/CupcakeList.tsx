40fcbc15d896011924077caae1b46da50e956740
jeremy gontier <jeremy.gontier@gmail.com>
2025-12-01 12:19:48 +0100

    step 5&6 terminé

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -1,11 +1,30 @@
 import { useEffect, useState } from "react";
 import Cupcake from "../components/Cupcake";
 
-type AccessoryArray = { id: number; name: string; slug: string }[];
+interface Accessory {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+interface CupcakeTypes {
+  id: number;
+  name: string;
+  accessory_id: string;
+  accessory: string;
+  color1: string;
+  color2: string;
+  color3: string;
+}
 
 const CupcakeList = () => {
-  const [cupcakes, setCupcakes] = useState<Cupcake[]>([]);
-  const [accessories, setAccessories] = useState<AccessoryArray>([]);
+  const [cupcakes, setCupcakes] = useState<CupcakeTypes[]>([]);
+  const [accessories, setAccessories] = useState<Accessory[]>([]);
+  const [selectedAccessory, setSelectedAccessory] = useState<string>("");
+
+  const filteredCupcakes = selectedAccessory
+    ? cupcakes.filter((cupcake) => cupcake.accessory === selectedAccessory)
+    : cupcakes;
 
   useEffect(() => {
     fetch("http://localhost:3310/api/cupcakes")
@@ -18,7 +37,7 @@ const CupcakeList = () => {
     fetch("http://localhost:3310/api/accessories")
       .then((res) => res.json())
       .then((data) => {
-        setAccessories(data as AccessoryArray);
+        setAccessories(data);
         console.info("Accessories:", data);
       })
       .catch((err) => console.error(err));
@@ -29,9 +48,12 @@ const CupcakeList = () => {
       <h1>My cupcakes</h1>
       <form className="center">
         <label htmlFor="cupcake-select">
-          {/* Step 5: use a controlled component for select */}
           Filter by{" "}
-          <select id="cupcake-select">
+          <select
+            id="cupcake-select"
+            value={selectedAccessory}
+            onChange={(e) => setSelectedAccessory(e.target.value)}
+          >
             <option value="">---</option>
             {accessories.map((accessorie) => (
               <option key={accessorie.id} value={accessorie.slug}>
@@ -42,7 +64,7 @@ const CupcakeList = () => {
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
-        {cupcakes.map((cupcake) => (
+        {filteredCupcakes.map((cupcake) => (
           <li className="cupcake-item" key={cupcake.id}>
             <Cupcake data={cupcake} />
           </li>
@@ -51,7 +73,5 @@ const CupcakeList = () => {
     </>
   );
 };
-
-/* ************************************************************************* */
 
 export default CupcakeList;
