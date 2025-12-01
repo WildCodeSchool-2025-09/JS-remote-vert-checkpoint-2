7e5ae36b9128742b2c013f5f379a0c89fdcbac3d
Empro <Pro.emeric.m@gmail.com>
2025-12-01 10:50:01 +0100

    Initial implementation of algorithm 1

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -7,11 +7,23 @@ Exemples :
 * "aaaaabbbaa" et "a" -> 7
 * "bbacbaaa" et "c" -> 1
 * "bbcc" et "a" -> 0
+* Initialiser un compteur , puis Parcourir chaque lettre 
+* si la caractére === à la lettre alors le compteur augemente
+* retourner le compteur avec la lettre.
+* 
+* Pour tester faire npm run test:1 --workspace=algo
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+  let counter = 0;
+  for (let i = 0; i < givenString.length; i++) {
+    const character = givenString[i];
+    if (character === letter) {
+      counter = counter + 1;
+    }
+  }
+
+  return counter;
 }
 
 export default countLetters;
