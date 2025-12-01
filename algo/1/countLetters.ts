895ec1be82d51fcc6979e751b2c85207787cb9c6
Yuliana Krasulya <krasulyayuliana@gmail.com>
2025-12-01 09:58:35 +0100

    implement countLetters

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,17 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+  let count = 0;
+
+  for (let i = 0; i < givenString.length; i++) {
+    const currentChar = givenString[i];
+
+    if (currentChar === letter) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 export default countLetters;
