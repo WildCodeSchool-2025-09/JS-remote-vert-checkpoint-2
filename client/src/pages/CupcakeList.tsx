ad05353b5c68533c8abc18682e64732406e9bd6b
ClarissaCardarelli <ccardare@gmail.com>
2025-12-01 11:04:44 +0100

    finished steps 3, 4, 5

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -1,41 +1,6 @@
 import { useEffect, useState } from "react";
 import Cupcake from "../components/Cupcake";
 
-/* ************************************************************************* */
-const sampleCupcakes: CupcakeArray = [
-  {
-    id: 10,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "blue",
-    color2: "white",
-    color3: "red",
-    name: "France",
-  },
-  {
-    id: 11,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "yellow",
-    color2: "red",
-    color3: "black",
-    name: "Germany",
-  },
-  {
-    id: 27,
-    accessory_id: "5",
-    accessory: "christmas-candy",
-    color1: "yellow",
-    color2: "blue",
-    color3: "blue",
-    name: "Sweden",
-  },
-];
-
-/* you can use sampleCupcakes if you're stucked on step 1 */
-/* if you're fine with step 1, just ignore this ;) */
-/* ************************************************************************* */
-
 function CupcakeList() {
   const [cupcakes, setCupcakes] = useState<CupcakeArray>([]);
   const [accessories, setAccessories] = useState<AccessoriesArray>([]);
@@ -75,7 +40,7 @@ function CupcakeList() {
           .filter(
             (cupcake) =>
               selectedAccessory === "" ||
-              cupcake.accessory === selectedAccessory
+              cupcake.accessory === selectedAccessory,
           )
           .map((cupcake) => (
             <Cupcake data={cupcake} key={cupcake.id} />
