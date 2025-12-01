74bc1a8eb52973ae3b93e8352b72f2cd24a371d3
SAUVAGEOT Thibaut <sauvageot.thibaut@hotmail.fr>
2025-12-01 12:50:18 +0100

    Dernier commit avant pull, arret a l'etape 3

---
+++ server/src/pages/CupcakeList.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+
+type AccessoryArray = { id: number; name: string; slug: string }[];
+type Cupcake = {
+	id: number;
+	accessory_id: string;
+	accessory: string;
+	color1: string;
+	color2: string;
+	color3: string;
+	name: string;
+};
+
+function CupcakeList() {
+	const [cupcakes, setCupcakes] = useState<any[]>([]);
+	const [accessories, setAccessories] = useState<AccessoryArray>([]);
+
+	useEffect(() => {
+		fetch("http://localhost:3310/api/cupcakes")
+			.then((res) => res.json())
+			.then((data) => {
+				setCupcakes(data);
+				console.info("Cupcakes reçus :", data);
+			})
+			.catch((err) => console.error("Erreur cupcakes :", err));
+	}, []);
+
+	useEffect(() => {
+		fetch("http://localhost:3310/api/accessories")
+			.then((res) => res.json())
+			.then((data) => {
+				setAccessories(data as AccessoryArray);
+				console.info("Accessoires reçus :", data);
+			})
+			.catch((err) => console.error("Erreur accessoires :", err));
+	}, []);
+
+	return React.createElement(
+		"div",
+		null,
+		React.createElement("h1", null, "Liste des Cupcakes"),
+		React.createElement(
+			"ul",
+			null,
+			cupcakes.map((cupcake) =>
+				React.createElement(
+					"li",
+					{ key: cupcake.id },
+					`${cupcake.name} – ${cupcake.accessory} (${cupcake.color1}, ${cupcake.color2}, ${cupcake.color3})`,
+				),
+			),
+		),
+		React.createElement("h1", null, "Liste des Accessoires"),
+		React.createElement(
+			"ul",
+			null,
+			accessories.map((acc) =>
+				React.createElement("li", { key: acc.id }, `${acc.name} (${acc.slug})`),
+			),
+		),
+		React.createElement("h1", null, "Sélection d’un accessoire"),
+		React.createElement(
+			"select",
+			{ id: "cupcake-select" },
+			React.createElement("option", { value: "" }, "---"),
+			accessories.map((acc) =>
+				React.createElement("option", { key: acc.id, value: acc.id }, acc.name),
+			),
+		),
+	);
+}
+
+export default CupcakeList;
--- server/src/router.ts
+++ server/src/router.ts
@@ -2,8 +2,29 @@ import express from "express";
 
 const router = express.Router();
 
+export const cupcakes = [
+	{
+		id: 1,
+		name: "Austria",
+		accessory: "chocolate",
+		color1: "red",
+		color2: "white",
+		color3: "red",
+	},
+	{
+		id: 2,
+		name: "Belgium",
+		accessory: "chocolate",
+		color1: "black",
+		color2: "yellow",
+		color3: "red",
+	},
+];
+
 /* ************************************************************************* */
-// Define Your API Routes Here
+router.get("/api/cupcakes", (req, res) => {
+	res.json(cupcakes);
+});
 /* ************************************************************************* */
 
 // Define item-related routes
