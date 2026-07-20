f038b1a9281a1d202284880db8617acdfbd02298
czagoh <dorian.pennegat@hotmail.fr>
2026-07-20 12:39:01 +0200

    Algo 1&2 Done, Step 1 to 5 Done

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -11,8 +11,22 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
 function getFibonacciSequence(size: number): number[] {
-  // Ton code ici !
-  return [0, 1];
+  if (size <= 0) {
+    return [];
+  }
+  if (size === 1) {
+    return [0];
+  }
+
+  const sequence = [0, 1];
+
+  for (let i = 2; i < size; i++) {
+    const nextNumber =
+      sequence[sequence.length - 1] + sequence[sequence.length - 2];
+    sequence.push(nextNumber);
+  }
+
+  return sequence;
 }
 
 export default getFibonacciSequence;
--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -1,35 +1,37 @@
+import { useEffect, useState } from "react";
 import Cupcake from "../components/Cupcake";
 
 /* ************************************************************************* */
-const sampleCupcakes: CupcakeArray = [
-  {
-    id: 10,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "blue",
-    color2: "white",
-    color3: "red",
-    name: "France",
-  },
-  {
-    id: 11,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "yellow",
-    color2: "red",
-    color3: "black",
-    name: "Germany",
-  },
-  {
-    id: 27,
-    accessory_id: "5",
-    accessory: "christmas-candy",
-    color1: "yellow",
-    color2: "blue",
-    color3: "blue",
-    name: "Sweden",
-  },
-];
+type AccessoryArray = { id: number; name: string; slug: string }[];
+// const sampleCupcakes: CupcakeArray = [
+//   {
+//     id: 10,
+//     accessory_id: "4",
+//     accessory: "wcs",
+//     color1: "blue",
+//     color2: "white",
+//     color3: "red",
+//     name: "France solong",
+//   },
+//   {
+//     id: 11,
+//     accessory_id: "4",
+//     accessory: "wcs",
+//     color1: "yellow",
+//     color2: "red",
+//     color3: "black",
+//     name: "Germany MDR Paraguay ?",
+//   },
+//   {
+//     id: 27,
+//     accessory_id: "5",
+//     accessory: "christmas-candy",
+//     color1: "yellow",
+//     color2: "blue",
+//     color3: "blue",
+//     name: "Sweden dur sans Zlatan",
+//   },
+// ];
 
 /* you can use sampleCupcakes if you're stucked on step 1 */
 /* if you're fine with step 1, just ignore this ;) */
@@ -37,10 +39,35 @@ const sampleCupcakes: CupcakeArray = [
 
 function CupcakeList() {
   // Step 1: get all cupcakes
+  const [cupcakes, setCupcakes] = useState<CupcakeArray>([]);
 
+  useEffect(() => {
+    const fetchCupcakes = async () => {
+      const response = await fetch("http://localhost:3310/api/cupcakes");
+      const data = await response.json();
+      console.info(data);
+      setCupcakes(data);
+    };
+
+    fetchCupcakes();
+  }, []);
   // Step 3: get all accessories
+  const [accessories, setAccessories] = useState<AccessoryArray>([]);
+  useEffect(() => {
+    const fetchAccessories = async () => {
+      const response = await fetch("http://localhost:3310/api/accessories");
+      const data = await response.json();
+      console.info(data);
+      setAccessories(data);
+    };
+    fetchAccessories();
+  }, []);
 
-  // Step 5: create filter state
+  const [selectedAccessory, setSelectedAccessory] = useState("");
+
+  const filteredCupcakes = selectedAccessory
+    ? cupcakes.filter((cupcake) => cupcake.accessory_id === selectedAccessory)
+    : cupcakes;
 
   return (
     <>
@@ -49,18 +76,27 @@ function CupcakeList() {
         <label htmlFor="cupcake-select">
           {/* Step 5: use a controlled component for select */}
           Filter by{" "}
-          <select id="cupcake-select">
+          <select
+            id="cupcake-select"
+            value={selectedAccessory}
+            onChange={(event) => setSelectedAccessory(event.target.value)}
+          >
             <option value="">---</option>
-            {/* Step 4: add an option for each accessory */}
+            {accessories.map((accessory) => (
+              <option key={accessory.id} value={accessory.id}>
+                {accessory.name}
+              </option>
+            ))}
           </select>
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
-        {/* Step 2: repeat this block for each cupcake */}
-        {/* Step 5: filter cupcakes before repeating */}
-        <li className="cupcake-item">
-          <Cupcake data={sampleCupcakes[0]} />
-        </li>
+        {filteredCupcakes.map((cupcake) => (
+          <li className="cupcake-item" key={cupcake.id}>
+            <Cupcake data={cupcake} />
+          </li>
+        ))}
+
         {/* end of block */}
       </ul>
     </>
