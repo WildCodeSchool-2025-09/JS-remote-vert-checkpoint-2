6025a64ed453458c5406ab13ac57a75e60e1baa6
Cynthiou <cynthia.flores@hotmail.fr>
2026-07-20 11:02:12 +0200

    finished step 1

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Cupcake from "../components/Cupcake";
 
 /* ************************************************************************* */
@@ -31,37 +32,51 @@ const sampleCupcakes: CupcakeArray = [
   },
 ];
 
-/* you can use sampleCupcakes if you're stucked on step 1 */
-/* if you're fine with step 1, just ignore this ;) */
+/* tu peux utiliser sampleCupcakes si tu bloques à l'étape 1 */
+/* si l'étape 1 est déjà faite, ignore simplement cette partie ;) */
 /* ************************************************************************* */
 
 function CupcakeList() {
-  // Step 1: get all cupcakes
+  const [cupcakes, setCupcakes] = useState<CupcakeArray>([]);
 
-  // Step 3: get all accessories
+  useEffect(() => {
+    fetch("http://localhost:3310/api/cupcakes")
+      .then((reponse) => reponse.json())
+      .then((data) => {
+        console.info(data);
+        setCupcakes(data);
+      });
+  }, []);
 
-  // Step 5: create filter state
+  // Étape 3 : récupérer tous les accessoires
+
+  // Étape 5 : créer un état (state) pour le filtre
 
   return (
     <>
       <h1>My cupcakes</h1>
+
       <form className="center">
         <label htmlFor="cupcake-select">
-          {/* Step 5: use a controlled component for select */}
+          {/* Étape 5 : utiliser un composant contrôlé pour le select */}
           Filter by{" "}
           <select id="cupcake-select">
             <option value="">---</option>
-            {/* Step 4: add an option for each accessory */}
+
+            {/* Étape 4 : ajouter une option pour chaque accessoire */}
           </select>
         </label>
       </form>
+
       <ul className="cupcake-list" id="cupcake-list">
-        {/* Step 2: repeat this block for each cupcake */}
-        {/* Step 5: filter cupcakes before repeating */}
+        {/* Étape 2 : répéter ce bloc pour chaque cupcake */}
+
+        {/* Étape 5 : filtrer les cupcakes avant de les afficher */}
         <li className="cupcake-item">
           <Cupcake data={sampleCupcakes[0]} />
         </li>
-        {/* end of block */}
+
+        {/* fin du bloc */}
       </ul>
     </>
   );
